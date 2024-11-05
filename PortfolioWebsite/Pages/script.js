8717a6b9e92aece8c3ffe8e2be2dd6a3ddacb612
Gomolemo Motlhamme <motlhammegomolemo@gmail.com>
2024-11-05 19:55:00 +0200

    rearanging

--- PortfolioWebsite/Pages/script.js
+++ PortfolioWebsite/Pages/script.js
@@ -10,15 +10,25 @@ function hideSidebar(){
 }
 // Select the content area you want to blur
 const homeContent = document.querySelector('.home-content');
+const home2ndSection = document.querySelector('.home-2ndSection');
 
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY; // Get current scroll position
-    const blurThreshold = 90; // Adjust this threshold as needed
+    const blurThreshold = 100; // Adjust this threshold as needed
+    const secblurThreshold = 360;
 
     // Apply blur if the scroll is past the threshold
     if (scrollPosition > blurThreshold) {
         homeContent.classList.add('blur-content');
+        
     } else {
         homeContent.classList.remove('blur-content');
+        
+    }
+
+    if (scrollPosition > secblurThreshold){
+        home2ndSection.classList.remove('blur-content');
+    } else {
+        home2ndSection.classList.add('blur-content');
     }
 });
