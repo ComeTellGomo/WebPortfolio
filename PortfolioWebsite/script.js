29fff92d4836cb779cdba8ae5b7552ef9952bd9a
HitekGomo <gomolemo.mothlamme@hiteknologysolutions.co.za>
2024-11-05 18:02:08 +0200

    blur effect and linkedin

--- PortfolioWebsite/script.js
+++ PortfolioWebsite/script.js
@@ -8,3 +8,17 @@ function hideSidebar(){
     sidebar.style.display = 'none';
 
 }
+// Select the content area you want to blur
+const homeContent = document.querySelector('.home-content');
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY; // Get current scroll position
+    const blurThreshold = 90; // Adjust this threshold as needed
+
+    // Apply blur if the scroll is past the threshold
+    if (scrollPosition > blurThreshold) {
+        homeContent.classList.add('blur-content');
+    } else {
+        homeContent.classList.remove('blur-content');
+    }
+});
