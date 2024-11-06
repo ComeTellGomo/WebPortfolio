fe889ad6df0a00b0a0b0cb0986afcabb26d77363
Gomolemo Motlhamme <motlhammegomolemo@gmail.com>
2024-11-06 21:57:24 +0200

    Adding logo and headshot

--- PortfolioWebsite/Pages/script.js
+++ PortfolioWebsite/Pages/script.js
@@ -1,3 +1,4 @@
+// sidebar functionality
 function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -8,22 +9,26 @@ function hideSidebar(){
     sidebar.style.display = 'none';
 
 }
-// Select the content area you want to blur
+
+
+// Select the content area to blur
 const homeContent = document.querySelector('.home-content');
+const theHeadShot = document.querySelector('.headshot');
 const home2ndSection = document.querySelector('.home-2ndSection');
+
 
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY; // Get current scroll position
-    const blurThreshold = 100; // Adjust this threshold as needed
+    const blurThreshold = 100; // Adjust threshold 
     const secblurThreshold = 360;
 
     // Apply blur if the scroll is past the threshold
     if (scrollPosition > blurThreshold) {
         homeContent.classList.add('blur-content');
-        
+        theHeadShot.classList.add('blur-content');
     } else {
         homeContent.classList.remove('blur-content');
-        
+        theHeadShot.classList.remove('blur-content');
     }
 
     if (scrollPosition > secblurThreshold){
